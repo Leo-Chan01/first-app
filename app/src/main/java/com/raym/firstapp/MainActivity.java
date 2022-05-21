bcaf5c6e1e15656cc02b8129d5ce8c6a82e2c4fd
Leo-Chan01 <ephraimleo16@gmail.com>
2022-05-21 23:06:10 +0100

    Added Code to make the click of the button increment the counter.

--- app/src/main/java/com/raym/firstapp/MainActivity.java
+++ app/src/main/java/com/raym/firstapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.IntegerRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -14,34 +15,37 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.raym.firstapp.databinding.ActivityMainBinding;
+import com.raym.firstapp.modelclasses.MyWorker;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
+        TextView textValue = findViewById(R.id.text_value);
 
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        binding.fab.setOnClickListener(view ->  {
+
+            String stringValue = textValue.getText().toString();
+            int originalValue = Integer.parseInt(stringValue);
+
+            int newValue = MyWorker.doubleTheValue(originalValue);
+            textValue.setText(String.valueOf(newValue));
+
+            Snackbar.make(view, "You have increased "
+                    + stringValue + " to " + newValue, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
         });
     }
 
@@ -66,11 +70,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-//        return NavigationUI.navigateUp(navController, appBarConfiguration)
-//                || super.onSupportNavigateUp();
-//    }
 }
---
+++ app/src/main/java/com/raym/firstapp/modelclasses/MyWorker.java
@@ -0,0 +1,7 @@
+package com.raym.firstapp.modelclasses;
+
+public class MyWorker {
+    public static int doubleTheValue(int value) {
+        return value * 2;
+    }
+}
